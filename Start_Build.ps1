ce70e9542256657b4e7a7be7f6dee1c2a010fb1c
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-06 20:49:31 +0200

    file renaming (in the script)

--- Start_Build.ps1
+++ Start_Build.ps1
@@ -10,7 +10,7 @@ mkdir ./Build/Temp/
 cp -r ./Wrappers/NodeJs/openipc-node/* ./Build/Temp/
 cp ./Wrappers/HtmlJs/index.js ./Build/Temp/ipclient.js
 cd ./Build/Temp/
-browserify ./ipclient.js -o ../OpenIPC-Client.js --plugin tinyify
+browserify ./ipclient.js -o ../OpenIPC-HtmlJs.js --plugin tinyify
 cd ../../
 
 rm -r ./Build/Temp/*
@@ -18,6 +18,6 @@ cp -r ./Wrappers/NodeJs/openipc-node/* ./Build/Temp/
 rm -r ./Build/Temp/node_modules/
 rm ./Build/Temp/.gitignore
 rm ./Build/Temp/package-lock.json
-Compress-Archive -Path ./Build/Temp/* -DestinationPath ./Build/OpenIPC-Node.zip
+Compress-Archive -Path ./Build/Temp/* -DestinationPath ./Build/OpenIPC-NodeJs.zip
 
-rm -r ./Build/Temp/*
+rm -r ./Build/Temp/
